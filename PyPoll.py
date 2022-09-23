f633081fc0487c46eac2e229ee9cee027f9d0ca0
CrystalRae90 <crystal.ho@libertyfrac.com>
2022-09-22 21:37:15 -0500

    Adding the election audit code.

--- PyPoll.py
+++ PyPoll.py
@@ -8,24 +8,69 @@
 import csv
 import os
 
-# Assign a variable for the file to load and the path.
+# Assign a variable to load a file from a path.
 file_to_load = os.path.join("Resources", "election_results.csv")
+
+# Assign a variable to save the file to a path.
+file_to_save = os.path.join("analysis", "election_analysis.txt")
+
+# 1. Initialize a total vote counter.
+total_votes = 0
+
+candidate_options =[]
+candidate_votes = {}
+
+winning_candidate =""
+winning_count = 0
+winning_percentage = 0
 
 # Open the election results and read the file.
 with open(file_to_load) as election_data:
 
-# To do: perform analysis.
-    print(election_data)
+    file_reader = csv.reader(election_data)
 
-# Create a filename variable to a direct or indirect path to the file.
+    #Print the header row 
+    headers = next(file_reader)
+  
+  # Print each row in the CSV file.
+    for row in file_reader:
+        total_votes += 1
 
-file_to_save = os.path.join("analysis", "election_analysis.txt")
+     # Print the candidate name from each row.
+        candidate_name = row[2]
+    
+        #Add the candidate name to the candidate list.
+        if candidate_name not in candidate_options:
+            candidate_options.append(candidate_name)
 
-# Using the open() function with the "w" mode we will write data to the file.
+            candidate_votes[candidate_name]=0
 
-outfile = open(file_to_save, "w")
+        candidate_votes[candidate_name] += 1
 
-outfile.write('hello world')
+    print(candidate_votes)
 
-outfile.close()
+    for candidate_name in candidate_votes : 
 
+        votes = candidate_votes[candidate_name]
+
+        vote_percentage = float(votes) / float(total_votes) *100     
+
+       # print(f"{candidate_name}: received {vote_percentage:.1f}% of the vote.")
+
+    # 1. Determine if the votes are greater than the winning count.
+        if (votes > winning_count) and (vote_percentage > winning_percentage):
+     # 2. If true then set winning_count = votes and winning_percent # vote_percentage.
+            winning_count = votes
+            winning_percentage = vote_percentage
+     # 3. Set the winning_candidate equal to the candidate's name.
+            winning_candidate = candidate_name
+
+        print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+
+    winning_candidate_summary = (
+    f"-------------------------\n"
+    f"Winner: {winning_candidate}\n"
+    f"Winning Vote Count: {winning_count:,}\n"
+    f"Winning Percentage: {winning_percentage:.1f}%\n"
+    f"-------------------------\n")
+    print(winning_candidate_summary)
--- Python_Practice.py
+++ Python_Practice.py
@@ -1,5 +1,1 @@
-counties = ["Arapahoe","Denver","Jefferson"]
-if "Arapahoe" in counties or "El Paso" in counties:
-    print("Arapahoe and El Paso are in the list of counties.")
-else:
-    print("Arapahoe or El Paso is not in the list of counties.")
+import csv
